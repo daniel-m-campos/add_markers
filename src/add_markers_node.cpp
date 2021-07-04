02ba32d699f9a7574c79efa0884f35a5b69ba211
Daniel M Campos <daniel.m.campos@icloud.com>
2021-07-03 21:06:11 -0500

    Make not_moving_threshold a default node parameter

--- src/add_markers_node.cpp
+++ src/add_markers_node.cpp
@@ -3,6 +3,14 @@
 
 #include "marker_manager.h"
 
+unsigned int GetNotMovingThreshold(const ros::NodeHandle &node_handle) {
+  int i = 100;
+  if (node_handle.getParam("~not_moving_threshold", i))
+    ROS_INFO_STREAM("Parameter not_moving_threshold set to" << i);
+  unsigned int not_moving_threshold = i;
+  return not_moving_threshold;
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle node_handle;
@@ -11,6 +19,8 @@ int main(int argc, char **argv) {
   position.x = -3.0;
   position.y = -1.2;
   orientation.w = 1.0;
-  MarkerManager manager{node_handle, position, orientation, 5, 100};
+  MarkerManager
+      manager{node_handle, position, orientation, 5,
+              GetNotMovingThreshold(node_handle)};
   ros::spin();
 }
